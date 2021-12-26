12c8cee8fa03e4d329bc3f764945d137b755c05a
Administrator <admin@example.com>
2021-12-26 08:24:31 +0800

    修改dynamic.d.ts
    Signed-off-by: Administrator <admin@example.com>

--- packages/dva/dynamic.d.ts
+++ packages/dva/dynamic.d.ts
@@ -1,2 +1,10 @@
-declare const dynamic: (resolve: (value?: PromiseLike<any>) => void) => void;
+import { DvaInstance } from "dva";
+import { ComponentType } from "react";
+
+declare const dynamic: (opts: {
+  app: DvaInstance,
+  models?: () => Array<PromiseLike<any>>,
+  component: () => PromiseLike<any>,
+}) => ComponentType<any>;
+
 export default dynamic;
